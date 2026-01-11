2773e2377ab4606057e8b19a4a77cf225900b897
ruben.araujo <62398031+rxbxn@users.noreply.github.com>
2026-01-10 19:23:14 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
-    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY || '',
+    // NUNCA exponer SUPABASE_SERVICE_ROLE_KEY al frontend
   }
 }
 
